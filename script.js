511635fcb97ea76f1c3dbf14fe1d08140376134b
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2022-01-11 23:43:53 -0800

    Data Types

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-alert("Salman");
-alert("Muhammad Salman");
-alert("JavaScript");
+// alert("Salman");
+// alert("Muhammad Salman");
+// alert("JavaScript");
+
+// Data Types
+String 
+    ("Hello")
+Number
+    (123)
+Boolean
+    ture  false
