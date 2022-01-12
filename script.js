0a76bc86b3630cf674393ab3257ca768e8db73d6
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2022-01-12 00:43:08 -0800

    String Concatenation

--- script.js
+++ script.js
@@ -17,21 +17,26 @@
 
 // Variable (var)
 
-var myName = "Muhammad Salman";
-console.log(myName);
-alert(myName);
+// var myName = "Muhammad Salman";
+// console.log(myName);
+// alert(myName);
 
-var myCity = "Faisalabad";
-console.log(myCity);
-alert(myCity);
+// var myCity = "Faisalabad";
+// console.log(myCity);
+// alert(myCity);
 
-var yourName = prompt("What is your Father's Name?");
-console.log(yourName);
-alert("My Name is"  +  myName  + "Welcome to My Home" + yourName + "!");
+// var yourName = prompt("What is your Father's Name?");
+// console.log(yourName);
+// alert("My Name is"  +  myName  + "Welcome to My Home" + yourName + "!");
 
-var gameLevel = 1;
-gameLevel = 2;
-gameLevel = 3;
-gameLevel = 4;
-gameLevel = 5;
-alert("Your currently level is " + gameLevel);
+// var gameLevel = 1;
+// gameLevel = 2;
+// gameLevel = 3;
+// gameLevel = 4;
+// gameLevel = 5;
+// alert("Your currently level is " + gameLevel);
+
+var message = "Hello";
+var myName = "Salman";
+
+alert(message + " There," + " " + myName);
