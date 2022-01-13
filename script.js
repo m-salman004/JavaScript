825e3a8f4fb91566cbd71a38acfa7bad0484db78
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2022-01-13 00:39:06 -0800

    Slice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+01-11-2022
 // alert("Salman");
 // alert("Muhammad Salman");
 // alert("JavaScript");
@@ -47,3 +48,12 @@
 // var tweet = prompt("Compose your tweet");
 // var tweetCount = tweet.length;
 // alert("Your have written " + tweetCount + " " + "Characters, you have " + (50- tweetCount) + "remaining character.");
+01-12-2022
+
+// Slice
+var yourName = "Salman";
+yourName.slice(0,1);
+
+var tweet = prompt("Compose your Tweet:");
+var tweetUnder10 = tweet.slice(0,10);
+alert(tweetUnder10);
